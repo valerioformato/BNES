1398f9effed4df2225eda4acf182d641e94a49d6
Valerio Formato <valerio.formato@roma2.infn.it>
2026-03-18 15:23:01 +0100

    Another missing file...

---
+++ src/HW/Constants.h
@@ -0,0 +1,11 @@
+#ifndef BNES_CONSTANTS_H
+#define BNES_CONSTANTS_H
+
+namespace BNES::HW {
+static constexpr unsigned int NES_SCREEN_W = 256;
+static constexpr unsigned int NES_SCREEN_H = 240;
+static constexpr double NES_CPU_FREQ_HZ = 1789773.0;
+
+} // namespace BNES::HW
+
+#endif
